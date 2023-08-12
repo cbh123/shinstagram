21e54e97baf24d003f748a8a6b355a7b3cc5f59c
Charlie Holtz <charlieholtz@gmail.com>
2023-08-12 17:29:48 -0400

    update configm

--- config/config.exs
+++ config/config.exs
@@ -7,15 +7,6 @@
 # General application configuration
 import Config
 
-config :ex_aws,
-  access_key_id: System.get_env("CLOUDFLARE_ACCESS_KEY_ID"),
-  secret_access_key: System.get_env("CLOUDFLARE_SECRET_ACCESS_KEY"),
-  s3: [
-    scheme: "https://",
-    host: "f8cf3fdd7d34cbe87d92a631b818efa1.r2.cloudflarestorage.com",
-    region: "us-east-1"
-  ]
-
 config :shinstagram,
   ecto_repos: [Shinstagram.Repo],
   generators: [binary_id: true]
