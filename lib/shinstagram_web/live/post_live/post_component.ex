12f866d5ffc6b18f8635d7825959c50be607de7d
Charlie Holtz <charlieholtz@gmail.com>
2023-08-12 16:31:34 -0400

    cleaner

---
+++ lib/shinstagram_web/live/post_live/post_component.ex
@@ -0,0 +1,54 @@
+defmodule ShinstagramWeb.PostLive.PostComponent do
+  use ShinstagramWeb, :live_component
+
+  alias Shinstagram.Timeline
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div id={@id} class="max-w-md mx-auto border-b pb-6 border-gray-300">
+      <.link navigate={~p"/#{@profile.username}"} class="group block flex-shrink-0">
+        <div class="flex items-center">
+          <div>
+            <img class="inline-block h-9 w-9 rounded-full" src={@profile.profile_photo} alt="" />
+          </div>
+          <div class="ml-3">
+            <p class="text-base font-semibold text-gray-900 group-hover:text-gray-700">
+              <%= @profile.username %>
+              <span class="text-xs text-gray-500">• <%= Timex.from_now(@post.inserted_at) %></span>
+            </p>
+            <p class="text-xs font-medium text-gray-500"><%= @post.location %></p>
+          </div>
+        </div>
+      </.link>
+      <div class="mt-4">
+        <img class="rounded-sm" src={@post.photo} alt={@post.photo_prompt} loading="lazy" />
+
+        <%!-- Reactions --%>
+        <button phx-click="like" phx-value-post_id={@post.id}>
+          <.icon name="hero-heart" class="h-6 w-6 mt-3 hover:text-gray-500" />
+        </button>
+
+        <%!-- Caption --%>
+        <p class="text-base mt-2">
+          <.link class="font-bold" navigate={~p"/#{@profile.username}"}>
+            <%= @profile.username %>
+          </.link>
+          <%= @post.caption |> String.replace("\"", "") %>
+        </p>
+
+        <p>
+          <%= for like <- Shinstagram.Timeline.get_likes_by_post_id(@post.id) do %>
+            <.link
+              class="hover:underline"
+              navigate={~p"/#{Shinstagram.Profiles.get_profile!(@post.profile_id).username}"}
+            >
+              <%= Shinstagram.Profiles.get_profile!(like.profile_id).username %>
+            </.link>
+          <% end %>
+        </p>
+      </div>
+    </div>
+    """
+  end
+end
