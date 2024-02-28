fbf76fb129e9d11fcf5f0eb3468a4ec4a2ec4796
Xiangzhuang Shen <datahonor@gmail.com>
2024-02-28 21:00:14 +0800

    fix: duplicate migrate name

--- config/config.exs
+++ config/config.exs
@@ -32,7 +32,7 @@ config :shinstagram, Shinstagram.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.14.41",
+  version: "0.17.11",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
@@ -42,7 +42,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.2.4",
+  version: "3.3.2",
   default: [
     args: ~w(
       --config=tailwind.config.js
